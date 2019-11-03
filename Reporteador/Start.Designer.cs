07e5cf73cf808afb39c17ccbc26da6ff11d8f979
Luis Manuel <luisillo-09@hotmail.com>
2019-11-03 01:02:31 -0800

    La conección está lista
    La validación de campos está pendiente

--- Reporteador/Start.Designer.cs
+++ Reporteador/Start.Designer.cs
@@ -28,25 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.TextoX = new System.Windows.Forms.Label();
+            this.labelService = new System.Windows.Forms.Label();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // TextoX
+            // labelService
             // 
-            this.TextoX.AutoSize = true;
-            this.TextoX.Location = new System.Drawing.Point(375, 225);
-            this.TextoX.Name = "TextoX";
-            this.TextoX.Size = new System.Drawing.Size(91, 13);
-            this.TextoX.TabIndex = 0;
-            this.TextoX.Text = "Reporteador ABD";
+            this.labelService.AutoSize = true;
+            this.labelService.Location = new System.Drawing.Point(195, 52);
+            this.labelService.Name = "labelService";
+            this.labelService.Size = new System.Drawing.Size(46, 13);
+            this.labelService.TabIndex = 0;
+            this.labelService.Text = "Servidor";
             // 
-            // Form1
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Location = new System.Drawing.Point(198, 88);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(29, 13);
+            this.labelUser.TabIndex = 1;
+            this.labelUser.Text = "User";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(273, 52);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(100, 20);
+            this.textBoxServer.TabIndex = 2;
+            this.textBoxServer.Text = "localhost";
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(273, 88);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUser.TabIndex = 3;
+            this.textBoxUser.Text = "root";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(198, 133);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(273, 133);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 5;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(254, 190);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 6;
+            this.buttonConnect.Text = "Conectar";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // Start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.TextoX);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.textBoxServer);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.labelService);
+            this.Name = "Start";
             this.Text = "Reporteador";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -55,7 +119,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label TextoX;
+        private System.Windows.Forms.Label labelService;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonConnect;
     }
 }
 
--- Reporteador/Start.cs
+++ Reporteador/Start.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Reporteador
 {
@@ -15,6 +16,52 @@ namespace Reporteador
         public Start()
         {
             InitializeComponent();
+        }
+
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            /*
+             * Al hacer click en el botón Connectar intentará conectarse con MySQL
+             */
+
+            // obtenemos el valor de los textbox
+            string server = textBoxServer.Text;
+            string user = textBoxUser.Text;
+            string password = textBoxPassword.Text;
+
+            // cadena que incluye los datos de la conección
+            string connectionString = String.Format("Server={0};Uid={1};Password={2};", server, user, password);
+
+            // conección
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = connection.CreateCommand();
+
+            // testing conección
+            command.CommandText = "SHOW DATABASES;";
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    MessageBox.Show(String.Format("{0}", reader.GetValue(0).ToString()), "xd");
+                }
+            }
+            catch (Exception ex)
+            {
+                // si entramos aquí significa que la conección falló
+                MessageBox.Show(
+                    String.Format(
+                        "No se pudo conectar al servidor \"{0}\" con el usuario \"{1}\"\n\n{2}",
+                        server,
+                        user,
+                        ex.Message
+                    ),
+                    "Conección fallida",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
         }
     }
 }
